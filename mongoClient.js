c37211e7f17bedc4002ec1e549dfcfd076ccc12f
Debojyoti <debocognizant@gmail.com>
2017-08-26 04:20:12 -0700

    'Node_MongoDB_API'

---
+++ mongoClient.js
@@ -0,0 +1,32 @@
+//const mongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb'); //object destructring ES6
+
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (error, db) => {
+
+    if (error) {
+        return console.log("Unable to connect to databaase server");
+    } else
+        console.log("Connection succedded");
+
+    // db.collection('ToDoApp').insert({ Name: "Debojyoti", Title: "Talukdar" }, (err, result) => {
+    //  if (err) {
+    //  return console.log("unable to insert", err);
+    //}
+
+    //console.log("insert data ", result);
+
+
+    //});
+
+    db.collection('Users').insert({ _id: new ObjectID(), Name: "Debojyoti", Password: "1234" }, (err, response) => {
+
+        if (err) {
+            return console.log("unable to insert", err);
+        }
+
+        console.log("insert data ", response.ops[0]._id.getTimestamp());
+    });
+    db.close();
+
+});
